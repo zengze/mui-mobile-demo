663e74c9997216e7636b6521799accca43631d14
zengze <zengze@smart>
2017-12-06 11:34:55 +0800

    调整侧滑菜单栏样式

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -14,64 +14,62 @@ export class NavMenu extends Component {
     return (
       <div id="offCanvasWrapper" className="mui-off-canvas-wrap mui-draggable mui-slide-in">
         {/* 侧滑菜单部分 */}
-        <div id="offCanvasSide" className="mui-off-canvas-left">
+        <div id="offCanvasSide" className="mui-off-canvas-left" style={{ background: '#001529' }}>
         	<div id="offCanvasSideScroll" className="mui-scroll-wrapper">
         		<div className="mui-scroll">
-        			<div style={{ margin: '10px 15px', textIndent: '1em', fontSize: 20, color: '#fff' }}>爱美斯国际物流</div>
-              <div className="mui-card">
-              	<ul className="mui-table-view">
-              		<li className="mui-table-view-cell mui-collapse">
-              			<a className="mui-navigate-right" href="#">
-                      <div className="mui-icon mui-icon-list" style={{ fontSize: 14, marginRight: 2 }}></div>
-                      组织机构管理
-                    </a>
-                    <ul className="mui-table-view">
-                      <li className="mui-table-view-cell">组织机构</li>
-                      <li className="mui-table-view-cell">添加组织</li>
-                    </ul>
-              		</li>
-              		<li className="mui-table-view-cell mui-collapse">
-              			<a className="mui-navigate-right" href="#">
-                      <div className="mui-icon mui-icon-list" style={{ fontSize: 14, marginRight: 2 }}></div>
-                      配置管理
-                    </a>
-                    <ul className="mui-table-view">
-                      <li className="mui-table-view-cell">配置列表</li>
-                      <li className="mui-table-view-cell">添加配置</li>
-                    </ul>
-              		</li>
-              		<li className="mui-table-view-cell mui-collapse">
-              			<a className="mui-navigate-right" href="#">
-                      <div className="mui-icon mui-icon-list" style={{ fontSize: 14, marginRight: 2 }}></div>
-                      角色管理
-                    </a>
-                    <ul className="mui-table-view">
-                      <li className="mui-table-view-cell">角色列表</li>
-                      <li className="mui-table-view-cell">添加角色</li>
-                    </ul>
-              		</li>
-              		<li className="mui-table-view-cell mui-collapse">
-              			<a className="mui-navigate-right" href="#">
-                      <div className="mui-icon mui-icon-list" style={{ fontSize: 14, marginRight: 2 }}></div>
-                      用户管理
-                    </a>
-                    <ul className="mui-table-view">
-                      <li className="mui-table-view-cell">用户列表</li>
-                      <li className="mui-table-view-cell">添加用户</li>
-                    </ul>
-              		</li>
-              		<li className="mui-table-view-cell mui-collapse">
-              			<a className="mui-navigate-right" href="#">
-                      <div className="mui-icon mui-icon-list" style={{ fontSize: 14, marginRight: 2 }}></div>
-                      视图管理
-                    </a>
-                    <ul className="mui-table-view">
-                      <li className="mui-table-view-cell">视图列表</li>
-                      <li className="mui-table-view-cell">添加视图</li>
-                    </ul>
-              		</li>
-              	</ul>
-              </div>
+        			<div style={{ padding: '12px 15px', fontSize: 20, color: '#fff', background: '#002140' }}>爱美斯国际物流</div>
+            	<ul className="mui-table-view">
+            		<li className="mui-table-view-cell mui-collapse">
+            			<a className="mui-navigate-right" href="#">
+                    <div className="mui-icon mui-icon-list" style={{ fontSize: 17, marginRight: 2 }}></div>
+                    组织机构管理
+                  </a>
+                  <ul className="mui-table-view">
+                    <li className="mui-table-view-cell">组织机构</li>
+                    <li className="mui-table-view-cell">添加组织</li>
+                  </ul>
+            		</li>
+            		<li className="mui-table-view-cell mui-collapse">
+            			<a className="mui-navigate-right" href="#">
+                    <div className="mui-icon mui-icon-list" style={{ fontSize: 17, marginRight: 2 }}></div>
+                    配置管理
+                  </a>
+                  <ul className="mui-table-view">
+                    <li className="mui-table-view-cell">配置列表</li>
+                    <li className="mui-table-view-cell">添加配置</li>
+                  </ul>
+            		</li>
+            		<li className="mui-table-view-cell mui-collapse">
+            			<a className="mui-navigate-right" href="#">
+                    <div className="mui-icon mui-icon-list" style={{ fontSize: 17, marginRight: 2 }}></div>
+                    角色管理
+                  </a>
+                  <ul className="mui-table-view">
+                    <li className="mui-table-view-cell">角色列表</li>
+                    <li className="mui-table-view-cell">添加角色</li>
+                  </ul>
+            		</li>
+            		<li className="mui-table-view-cell mui-collapse">
+            			<a className="mui-navigate-right" href="#">
+                    <div className="mui-icon mui-icon-list" style={{ fontSize: 17, marginRight: 2 }}></div>
+                    用户管理
+                  </a>
+                  <ul className="mui-table-view">
+                    <li className="mui-table-view-cell">用户列表</li>
+                    <li className="mui-table-view-cell">添加用户</li>
+                  </ul>
+            		</li>
+            		<li className="mui-table-view-cell mui-collapse">
+            			<a className="mui-navigate-right" href="#">
+                    <div className="mui-icon mui-icon-list" style={{ fontSize: 17, marginRight: 2 }}></div>
+                    视图管理
+                  </a>
+                  <ul className="mui-table-view">
+                    <li className="mui-table-view-cell">视图列表</li>
+                    <li className="mui-table-view-cell">添加视图</li>
+                  </ul>
+            		</li>
+            	</ul>
         		</div>
         	</div>
         </div>
